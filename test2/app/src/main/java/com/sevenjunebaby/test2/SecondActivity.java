dd07d936e8ab4a47eedc75ec408562532cea3f54
sevenjune <yahiawissal76@gmail.com>
2026-01-11 18:08:15 +0100

    update

---
+++ test2/app/src/main/java/com/sevenjunebaby/test2/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.sevenjunebaby.test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(v -> finish());
+    }
+}
